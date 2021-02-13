4944a284ce915f5c334f2293f8dffd0f10145303
DESKTOP-JRC8315\79527 <raketa.souz@gmail.com>
2021-02-13 16:59:38 +0300

    test3

--- pow.js
+++ pow.js
@@ -1,6 +1,11 @@
 function pow(x, n) {
-
-  let result = Math.pow(x, n);
+  let result;
+  
+  if ( x>0 && n>0 ) { 
+    result = Math.pow(x, n);
+  } else {
+    result = 'false'
+  }
   
   return result;
 }
